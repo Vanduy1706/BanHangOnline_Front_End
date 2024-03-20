8f6c1133ff555e3c6dd79b6d013f024534b643ad
Vo Van Duy <vovanduy170603@gmail.com>
2024-03-20 20:32:54 +0700

    Thêm chức năng đăng nhập bằng google

--- src/index.js
+++ src/index.js
@@ -7,21 +7,24 @@ import { persistor, store } from './redux/store'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { PersistGate } from 'redux-persist/integration/react';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import keys from './oauth2.keys.json'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient()
 root.render(
   // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
+  <GoogleOAuthProvider clientId={keys.web.client_id}>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </GoogleOAuthProvider>
   // </React.StrictMode>
 );
 
